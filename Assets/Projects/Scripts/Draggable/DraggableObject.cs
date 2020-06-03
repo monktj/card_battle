57b95ab94b7e9f65795a9546e1c0d1ae48d813b5
Sanghun Lee <tkdgns1284@gmail.com>
2020-06-03 21:54:01 +0900

    Drag 작업중

---
+++ Assets/Projects/Scripts/Draggable/DraggableObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableObject : MonoBehaviour,IDraggable
+{
+    public Vector3 dampingScale = Vector3.one;
+}
---
+++ Assets/Projects/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameTurn { 
+    Buy,Battle
+}
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager instance {
+        get {
+            if (_instance==null) {
+                _instance=FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public GameTurn gameTurn;
+}
